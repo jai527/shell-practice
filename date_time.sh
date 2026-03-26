83ae4e20dd66d5701cd3a19a276759e732923ce5
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-03-26 21:32:15 +0530

    all commit

--- date_time.sh
+++ date_time.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-timestamp=$(date)
+start time=$(date +%s)
 echo "script exicuted at :$timestamp"
+
+end_time=$(date +%s)
+total_time=$(($end_time-$start_time))
+echo "script exicuted in: $total_time"
+
