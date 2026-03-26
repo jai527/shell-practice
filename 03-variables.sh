f95cc49e8fbfe891583a0a24501301b581d34aa5
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-03-26 09:31:34 +0530

    hell

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+persion1=jai
+persion2=sangeetha
+echo "$persion1:: hi $persion2"
+echo "$persion2:: hi $persion1"
+echo "$persion1:: how are you $persion2"
+echo "$persion2:: iam fine $persion1 how are you"
+echo "$persion1:: iam gud $persion2"
