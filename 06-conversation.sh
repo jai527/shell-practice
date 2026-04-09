a243471be4ccf7e032bb3167a4d3e3721d60721e
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-04-09 09:13:19 +0530

    action

--- 06-conversation.sh
+++ 06-conversation.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-today = wednsday
-if (today ! = sunday)
-  print ("not go to school")
+today=wednsday
+if (today ! = sunday) {
+print ("not go to school")
+}
 elese{
    print ("go to school")
    }
