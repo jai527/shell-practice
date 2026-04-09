ad600c2a11dbffbb9c4657bc962315218b865242
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-04-09 15:40:25 +0530

    hello

--- 05-data-type.sh
+++ 05-data-type.sh
@@ -10,6 +10,6 @@ echo "sum: $sum"
 
 fruits=("banana apple orange mango")
   echo "all fruits: ${fruits[@]}"
-  echo "first fruit is ${fruits[0]}"
-  echo "second fruit is ${fruits[1]}"
-  echo "thired fruit is ${fruits[2]}"
+  echo "first fruit is: ${fruits[0]}"
+  echo "second fruit is: ${fruits[1]}"
+  echo "thired fruit is": ${fruits[2]}"`
