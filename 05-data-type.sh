96b23136cc9713dd513c7891456f1812dbdb23ef
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-04-09 15:46:32 +0530

    all

--- 05-data-type.sh
+++ 05-data-type.sh
@@ -12,4 +12,4 @@ fruits=("banana apple orange mango")
   echo "all fruits: ${fruits[@]}"
   echo "first fruit is: ${fruits[0]}"
   echo "second fruit is: ${fruits[1]}"
-  echo "thired fruit is": ${fruits[2]}"`
+  echo "thired fruit is: ${fruits[2]}"`
