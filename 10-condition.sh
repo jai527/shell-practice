163f0b89a02bb9028dd540f5d354d94c56f8d3de
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-04-09 17:15:35 +0530

    hi

--- 10-condition.sh
+++ 10-condition.sh
@@ -4,6 +4,9 @@ Number=$1
 
 if [ $Number -gt 20 ]; then
   echo "given number: $Number is greater then 20"
+
+  elif [ $Number -eq 20 ]; then
+  echo "given number: $Number is greater then 20"
   else 
    echo "give number: $Number is less then 20"
 
