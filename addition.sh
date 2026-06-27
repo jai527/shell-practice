193326f071d1acdff739b0eef9aca8dec556ec75
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-06-28 00:21:04 +0530

    all

---
+++ addition.sh
@@ -0,0 +1,21 @@
+a=10
+b=200
+sum=$(($a + $b))
+echo "addition of two valves $sum"
+product=$(($a * $b))
+echo "multiplie two valves $product"
+
+
+h=$1
+m=$2
+s=$3
+
+TS=$((h * 3600 + m * 60 + s))
+
+echo "enter the valve: $TS"
+
+name=$1
+name=$2
+name=$3
+
+echo "please geve me family members name $1 $2 $3"
